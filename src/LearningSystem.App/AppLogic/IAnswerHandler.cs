1be6dcfb496980f75764a4596022aef7a3d50b37
staafl <velko.nikolov@gmail.com>
2013-09-30 00:22:53 +0300

    initial

---
+++ src/LearningSystem.App/AppLogic/IAnswerHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSystem.App.AppLogic
+{
+    /// <summary>
+    /// Handles user input
+    /// </summary>
+    public interface IAnswerHandler
+    {
+        AnswerValidationResult ValidateInput(string input);
+
+        string RenderInputHtml();       
+    }
+}
