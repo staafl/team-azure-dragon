008e5746fac36052c9ae393d506fb18f54631a87
staafl <velko.nikolov@gmail.com>
2013-09-29 23:20:02 +0300

    added ApiController

---
+++ src/LearningSystem.App/Controllers/ApiController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TeamAzureDragon.Utils;
+
+namespace LearningSystem.App
+{
+    public class ApiController : System.Web.Http.ApiController
+    {
+        IUnitOfWork db;
+        public ApiController(IUnitOfWork db)
+        {
+            this.db = db;
+        }
+
+        public string Status()
+        {
+            return "System online";
+        }
+
+        [HttpPost]
+        public string WipeDatabase(string sure)
+        {
+            if (sure.ToLower() == "yes")
+            {
+                string[] tables =
+                {
+                    "dbo.AspNetUserRoles",
+                    "dbo.AspNetTokens",
+                    "dbo.AspNetUserClaims",
+                    "dbo.AspNetUserLogins",
+                    "dbo.AspNetUserManagement",
+                    "dbo.AspNetUserSecrets",
+                    "dbo.AspNetUsers",
+                    "dbo.AspNetRoles",
+                };
+                using (var context = new LearningSystemContext())
+
+                    foreach (var tableName in new string[0])
+                    {
+                        context.Database.ExecuteSqlCommand(string.Format("DELETE FROM {0}", tableName));
+                    }
+                return "Database successfully cleared.";
+            }
+            throw new HttpException(400, "");
+        }
+
+        [HttpPost]
+        public void Kill(string sure)
+        {
+            if (sure.ToLower() == "yes")
+            {
+                Environment.Exit(0);
+                throw new ApplicationException("Shouldn't happen");
+            }
+            throw new HttpException(400, "");
+        }
+        //// GET 
+        // api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<controller>
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
--- src/LearningSystem.App/Global.asax.cs
+++ src/LearningSystem.App/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -26,6 +27,10 @@ namespace LearningSystem.App
             }
 
             AreaRegistration.RegisterAllAreas();
+
+
+            // keep this first to ensure WebAPI routing
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
