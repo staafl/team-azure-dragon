9270acfc603b44a24c424a2eca88d0bf36b6d316
asenval <alisp.programs@gmail.com>
2013-09-30 14:27:04 +0300

    route correction

--- src/LearningSystem.App/App_Start/RouteConfig.cs
+++ src/LearningSystem.App/App_Start/RouteConfig.cs
@@ -13,10 +13,11 @@ namespace LearningSystem.App
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+               routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "LearningSystem.App.Controllers" }
             );
         }
     }
