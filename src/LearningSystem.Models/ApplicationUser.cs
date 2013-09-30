66fa68f82da32e8ca1608a0f112b8c6eb6c6cf77
asenval <alisp.programs@gmail.com>
2013-09-30 09:45:56 +0300

    Add some attribute validation for url email and not containing <script>

--- src/LearningSystem.Models/ApplicationUser.cs
+++ src/LearningSystem.Models/ApplicationUser.cs
@@ -10,8 +10,13 @@ namespace LearningSystem.Models
 {
     public class ApplicationUser : User
     {
+        [RegularExpression(@"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" + 
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,24}))$",
+                ErrorMessage="You must enter a valid e-mail address.")]
         public string Email { get; set; }
 
+        [RegularExpression(@"(www\.|http?://(www\.))?facebook\.com/(profile\.php\?id=)?([^/#?]+)(\n|$)",
+                ErrorMessage = "You must enter a valid Facebok profile link.")]
         public string Facebook { get; set; }
 
         [ScaffoldColumn(false)]
---
+++ src/LearningSystem.Models/StringNotContains.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace LearningSystem.Models
+{
+    public class StringNotContainsAttribute : ValidationAttribute
+    {
+        private string text;
+
+        public StringNotContainsAttribute(string text)
+        {
+            this.text = text;
+        }
+
+        public override bool IsValid(object value)
+        {
+            string valueAsString = value as string;
+            if (valueAsString == null)
+            {
+                return true;
+            }
+
+            return !valueAsString.Contains(text);
+        }
+    }
+}
