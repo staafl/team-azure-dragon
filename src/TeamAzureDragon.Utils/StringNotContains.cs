9d84812aa55a945ffbb6ce62432400279e39b335
asenval <alisp.programs@gmail.com>
2013-09-30 13:07:03 +0300

    some changes

--- src/TeamAzureDragon.Utils/StringNotContains.cs
+++ src/TeamAzureDragon.Utils/StringNotContains.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
-namespace LearningSystem.Models
+namespace TeamAzureDragon.Utils
 {
     public class StringNotContainsAttribute : ValidationAttribute
     {
