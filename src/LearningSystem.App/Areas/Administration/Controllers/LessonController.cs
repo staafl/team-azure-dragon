69375ecbbe8b7ed38a013fea2835d10b099b12cc
asenval <alisp.programs@gmail.com>
2013-09-30 18:12:41 +0300

    LessonController.cs

---
+++ src/LearningSystem.App/Areas/Administration/Controllers/LessonController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LearningSystem.Models;
+using LearningSystem.Data;
+
+namespace LearningSystem.App.Areas.Administration.Controllers
+{
+    public class LessonController : Controller
+    {
+        private LearningSystemContext db = new LearningSystemContext();
+
+        // GET: /Administration/Lesson/
+        public ActionResult Index()
+        {
+            var lessons = db.Lessons.Include(l => l.Skill);
+            return View(lessons.ToList());
+        }
+
+        // GET: /Administration/Lesson/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = db.Lessons.Find(id);
+            if (lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lesson);
+        }
+
+        // GET: /Administration/Lesson/Create
+        public ActionResult Create()
+        {
+            ViewBag.SkillId = new SelectList(db.Skills, "SkillId", "Name");
+            return View();
+        }
+
+        // POST: /Administration/Lesson/Create
+		// To protect from over posting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		// 
+		// Example: public ActionResult Update([Bind(Include="ExampleProperty1,ExampleProperty2")] Model model)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Lesson lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lessons.Add(lesson);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SkillId = new SelectList(db.Skills, "SkillId", "Name", lesson.SkillId);
+            return View(lesson);
+        }
+
+        // GET: /Administration/Lesson/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = db.Lessons.Find(id);
+            if (lesson == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SkillId = new SelectList(db.Skills, "SkillId", "Name", lesson.SkillId);
+            return View(lesson);
+        }
+
+        // POST: /Administration/Lesson/Edit/5
+		// To protect from over posting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		// 
+		// Example: public ActionResult Update([Bind(Include="ExampleProperty1,ExampleProperty2")] Model model)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Lesson lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lesson).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SkillId = new SelectList(db.Skills, "SkillId", "Name", lesson.SkillId);
+            return View(lesson);
+        }
+
+        // GET: /Administration/Lesson/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = db.Lessons.Find(id);
+            if (lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lesson);
+        }
+
+        // POST: /Administration/Lesson/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lesson lesson = db.Lessons.Find(id);
+            db.Lessons.Remove(lesson);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
