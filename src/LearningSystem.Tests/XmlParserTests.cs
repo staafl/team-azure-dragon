e63c3bf1ae78c72393e41202867adf11d1a018b4
Георги <georgi.ivanovtn@gmail.com>
2013-10-04 11:43:01 +0300

    added tests for correct parsing of skills and lessons

--- src/LearningSystem.Tests/XmlParserTests.cs
+++ src/LearningSystem.Tests/XmlParserTests.cs
@@ -27,19 +27,37 @@ namespace LearningSystem.Tests
 
         }
 
+        private byte[] LoadBytesFromFile(string path)
+        {
+            FileStream _stream = new FileStream(string.Format(path), FileMode.Open);
+            byte[] bytes = new byte[_stream.Length];
+            _stream.Read(bytes, 0, bytes.Length);
+            _stream.Close();
+
+            return bytes;
+        }
+
+        private static Mock<HttpPostedFileBase> MockAddMethod(MemoryStream memory)
+        {
+            var file = new Mock<HttpPostedFileBase>();
+
+            file.Setup(x => x.InputStream).Returns(memory);
+            file.Setup(x => x.ContentLength).Returns((int)memory.Length);
+            return file;
+        }
+
         [TestMethod]
         public void PlacesObjectInDb()
         {
             var mockDbProvider = new Mock<IUoWLearningSystem>();
 
-            FileStream _stream = new FileStream(string.Format("..//..//TestingZips//picking-mushrooms.zip"), FileMode.Open);
+            byte[] stream = LoadBytesFromFile("..//..//TestingZips//picking-mushrooms.zip");
 
-            var file = new Mock<HttpPostedFileBase>();
-            
-            file.Setup(x => x.InputStream).Returns(_stream);
-            file.Setup(x => x.ContentLength).Returns((int)_stream.Length);
-            file.Setup(x => x.FileName).Returns(_stream.Name);
+            MemoryStream memory = new MemoryStream(stream);
 
+
+            var file = MockAddMethod(memory);
+            //file.Setup(x => x.FileName).Returns(_stream.Name);
 
             mockDbProvider.Setup(x => x.Skills.Add(It.IsAny<Skill>()));
 
@@ -48,6 +66,64 @@ namespace LearningSystem.Tests
             controller.SaveSkill(file.Object);
 
             mockDbProvider.Verify(x => x.Skills.Add(It.IsAny<Skill>()));
+        }
+
+       
+
+        [TestMethod]
+        public void CorrectSkillProperties()
+        {
+            var mockDbProvider = new Mock<IUoWLearningSystem>();
+
+            byte[] stream = LoadBytesFromFile("..//..//TestingZips//picking-mushrooms.zip");
+            MemoryStream memory = new MemoryStream(stream);
+
+            var file = MockAddMethod(memory);
+
+            mockDbProvider.Setup(x => x.Skills.Add(It.IsAny<Skill>()));
+
+            UploadController controller = new UploadController(mockDbProvider.Object);
+
+            controller.SaveSkill(file.Object);
+
+            mockDbProvider.Verify(x => x.Skills.Add(It.Is<Skill>(
+                s =>
+                s.Name == "Mushroom Picking" &&
+                s.Description == "Learn how to pick mushrooms like a pro")));
+        }
+
+        [TestMethod]
+        public void CorrectLessonProperties()
+        {
+            var mockDbProvider = new Mock<IUoWLearningSystem>();
+
+            byte[] stream = LoadBytesFromFile("..//..//TestingZips//picking-mushrooms.zip");
+            MemoryStream memory = new MemoryStream(stream);
+
+            var file = MockAddMethod(memory);
+
+            mockDbProvider.Setup(x => x.Skills.Add(It.IsAny<Skill>()));
+
+            UploadController controller = new UploadController(mockDbProvider.Object);
+
+            controller.SaveSkill(file.Object);
+
+            mockDbProvider.Verify(x => x.Skills.Add(It.Is<Skill>(
+                s =>
+                s.Lessons.Count == 6 &&
+                s.Lessons.ElementAt(5).Requirements.First().LessonId == 4 &&
+                s.Lessons.ElementAt(5).Exercises.Single().ExerciseId == 7 &&
+                s.Lessons.ElementAt(5).LessonId == 6
+                )));
+
+            mockDbProvider.Verify(x => x.Skills.Add(It.Is<Skill>(
+                s =>
+                s.Lessons.ElementAt(1).LessonId == 2 &&
+                s.Lessons.ElementAt(1).Requirements.First().LessonId == 1 &&
+                s.Lessons.ElementAt(1).Exercises.ElementAt(0).ExerciseId == 2 &&
+                s.Lessons.ElementAt(1).Exercises.ElementAt(1).ExerciseId == 3 
+                )));
+            
         }
     }
 }
