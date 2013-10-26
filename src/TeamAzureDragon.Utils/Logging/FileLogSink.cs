7f899d2c8f41080dfec6edb6ae50c8b47ed0783b
staafl <velko.nikolov@gmail.com>
2013-10-27 01:04:48 +0300

    amend

--- src/TeamAzureDragon.Utils/Logging/FileLogSink.cs
+++ src/TeamAzureDragon.Utils/Logging/FileLogSink.cs
@@ -6,7 +6,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Web.Mvc;
 using System.Xml;
 using System.Threading;
 
--- src/TeamAzureDragon.Utils/Logging/Log.cs
+++ src/TeamAzureDragon.Utils/Logging/Log.cs
@@ -11,53 +11,6 @@ using System.Threading;
 
 namespace TeamAzureDragon.Utils.Log
 {
-    public interface ILogSink
-    {
-        void Log(string log, string message);
-        string RetrieveLog(string log);
-    }
-    
-    public class FileLogSink : ILogSink
-    {
-        public string FilePath { get; set; }
-        
-        string GetLogPath(string log) {
-            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, log + ".log");
-        }
-        
-        public string RetrieveLog(string log) {
-            return File.ReadAllText(GetLogPath(log));
-        }
-        
-        public void Log(string log, string message) {
-            File.AppendAllText(GetLogPath(log), message + Environment.NewLine + Environment.NewLine);
-        }
-    }
-    
-    public class ListLogSink : ILogSink
-    {
-        
-        readonly Dictionary<string, List<string>> logs = 
-             new Dictionary<string, List<string>>();
-        
-        List<string>  GetLogList(string log) {
-            log = log.ToUpper();
-            List<string> logList;
-            if (!logs.TryGetValue(log, out logList))
-            {
-                logs[log] = logList = new List<string>();
-            }
-            return logList;
-        }
-        public string RetrieveLog(string log) {
-            return string.Join("\n", GetLogList(log));
-        }
-        
-        public void Log(string log, string message) {
-            GetLogList(log).Add(message);
-        }
-    }
-    
     public static class Log
     {
         static int timeStampId = 0;
@@ -79,15 +32,13 @@ namespace TeamAzureDragon.Utils.Log
             Log.LogSink = logSink;
             Log.Trace("Logging started, using " + logSink.GetType().Name);
         }
-        
-        public static ITimeStampFactory TimeStampFactory { get; set; }
         public static ILogSink LogSink { get; set; }
         
         static readonly string NL = Environment.NewLine;
         
         public static string Trace(string message, string thread = null)
         {
-            var timeStamp = Misc.GetTimeStamp(DateTime.Now, true, true, true, true, true);
+            var timeStamp = GetTimeStamp(DateTime.Now, true, true, true, true, true);
             thread = thread ?? timeStamp;
             lock(locker) {
                 LogSink.Log("trace",
@@ -100,7 +51,7 @@ namespace TeamAzureDragon.Utils.Log
         }
         public static string Exception(Exception exception, string thread = null, string place = null)
         {
-            var timeStamp = Misc.GetTimeStamp(DateTime.Now, true, true, true, true, true);
+            var timeStamp = Log.GetTimeStamp(DateTime.Now, true, true, true, true, true);
             thread = thread ?? timeStamp;
             lock(locker) {
                 if (exception == null) {
